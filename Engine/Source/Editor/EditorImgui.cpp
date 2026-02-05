956c8671a0f8d7a06aed24df2356465da9b39f4d
info@vltmedia.com <info@vltmedia.com>
2026-02-05 05:05:39 -0500

    Chore: Cleaning up hardcoded colors

--- Engine/Source/Editor/EditorImgui.cpp
+++ Engine/Source/Editor/EditorImgui.cpp
@@ -72,7 +72,12 @@ struct FileBrowserDirEntry
 };
 
 static const float kSidePaneWidth = 200.0f;
-static const float kViewportBarHeight = 40.0f;
+static float GetViewportBarHeight()
+{
+    const ImGuiStyle& style = ImGui::GetStyle();
+    float buttonH = ImGui::GetFontSize() + style.FramePadding.y * 2.0f;
+    return buttonH + style.WindowPadding.y * 2.0f;
+}
 static const ImGuiWindowFlags kPaneWindowFlags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove;
 
 static const ImVec4 kSelectedColor = ImVec4(0.12f, 0.50f, 0.47f, 1.00f);
@@ -1197,97 +1202,6 @@ static void CreateNewAsset(TypeId assetType, const char* assetName)
     {
         AssetManager::Get()->SaveAsset(*stub);
     }
-}
-
- static void SetupImGuiStyle()
-{
-    // Future Dark style by rewrking from ImThemes
-    ImGuiStyle& style = ImGui::GetStyle();
-    
-    style.Alpha = 1.0f;
-    style.DisabledAlpha = 1.0f;
-    style.WindowPadding = ImVec2(12.0f, 12.0f);
-    style.WindowRounding = 0.0f;
-    style.WindowBorderSize = 0.0f;
-    style.WindowMinSize = ImVec2(20.0f, 20.0f);
-    style.WindowTitleAlign = ImVec2(0.5f, 0.5f);
-    style.WindowMenuButtonPosition = ImGuiDir_None;
-    style.ChildRounding = 0.0f;
-    style.ChildBorderSize = 1.0f;
-    style.PopupRounding = 0.0f;
-    style.PopupBorderSize = 1.0f;
-    style.FramePadding = ImVec2(6.0f, 6.0f);
-    style.FrameRounding = 0.0f;
-    style.FrameBorderSize = 0.0f;
-    style.ItemSpacing = ImVec2(12.0f, 6.0f);
-    style.ItemInnerSpacing = ImVec2(6.0f, 3.0f);
-    style.CellPadding = ImVec2(12.0f, 6.0f);
-    style.IndentSpacing = 20.0f;
-    style.ColumnsMinSpacing = 6.0f;
-    style.ScrollbarSize = 12.0f;
-    style.ScrollbarRounding = 0.0f;
-    style.GrabMinSize = 12.0f;
-    style.GrabRounding = 0.0f;
-    style.TabRounding = 0.0f;
-    style.TabBorderSize = 0.0f;
-    style.TabMinWidthForCloseButton = 0.0f;
-    style.ColorButtonPosition = ImGuiDir_Right;
-    style.ButtonTextAlign = ImVec2(0.5f, 0.5f);
-    style.SelectableTextAlign = ImVec2(0.0f, 0.0f);
-    
-    style.Colors[ImGuiCol_Text] = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
-    style.Colors[ImGuiCol_TextDisabled] = ImVec4(0.27450982f, 0.31764707f, 0.4509804f, 1.0f);
-    style.Colors[ImGuiCol_WindowBg] = ImVec4(0.078431375f, 0.08627451f, 0.101960786f, 1.0f);
-    style.Colors[ImGuiCol_ChildBg] = ImVec4(0.078431375f, 0.08627451f, 0.101960786f, 1.0f);
-    style.Colors[ImGuiCol_PopupBg] = ImVec4(0.078431375f, 0.08627451f, 0.101960786f, 1.0f);
-    style.Colors[ImGuiCol_Border] = ImVec4(0.15686275f, 0.16862746f, 0.19215687f, 1.0f);
-    style.Colors[ImGuiCol_BorderShadow] = ImVec4(0.078431375f, 0.08627451f, 0.101960786f, 1.0f);
-    style.Colors[ImGuiCol_FrameBg] = ImVec4(0.11764706f, 0.13333334f, 0.14901961f, 1.0f);
-    style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.15686275f, 0.16862746f, 0.19215687f, 1.0f);
-    style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.23529412f, 0.21568628f, 0.59607846f, 1.0f);
-    style.Colors[ImGuiCol_TitleBg] = ImVec4(0.047058824f, 0.05490196f, 0.07058824f, 1.0f);
-    style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.047058824f, 0.05490196f, 0.07058824f, 1.0f);
-    style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.078431375f, 0.08627451f, 0.101960786f, 1.0f);
-    style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.09803922f, 0.105882354f, 0.12156863f, 1.0f);
-    style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.047058824f, 0.05490196f, 0.07058824f, 1.0f);
-    style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.11764706f, 0.13333334f, 0.14901961f, 1.0f);
-    style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.15686275f, 0.16862746f, 0.19215687f, 1.0f);
-    style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.11764706f, 0.13333334f, 0.14901961f, 1.0f);
-    style.Colors[ImGuiCol_CheckMark] = ImVec4(0.49803922f, 0.5137255f, 1.0f, 1.0f);
-    style.Colors[ImGuiCol_SliderGrab] = ImVec4(0.49803922f, 0.5137255f, 1.0f, 1.0f);
-    style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.5372549f, 0.5529412f, 1.0f, 1.0f);
-    style.Colors[ImGuiCol_Button] = ImVec4(0.11764706f, 0.13333334f, 0.14901961f, 1.0f);
-    style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.19607843f, 0.1764706f, 0.54509807f, 1.0f);
-    style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.23529412f, 0.21568628f, 0.59607846f, 1.0f);
-    style.Colors[ImGuiCol_Header] = ImVec4(0.11764706f, 0.13333334f, 0.14901961f, 1.0f);
-    style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.19607843f, 0.1764706f, 0.54509807f, 1.0f);
-    style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.23529412f, 0.21568628f, 0.59607846f, 1.0f);
-    style.Colors[ImGuiCol_Separator] = ImVec4(0.15686275f, 0.18431373f, 0.2509804f, 1.0f);
-    style.Colors[ImGuiCol_SeparatorHovered] = ImVec4(0.15686275f, 0.18431373f, 0.2509804f, 1.0f);
-    style.Colors[ImGuiCol_SeparatorActive] = ImVec4(0.15686275f, 0.18431373f, 0.2509804f, 1.0f);
-    style.Colors[ImGuiCol_ResizeGrip] = ImVec4(0.11764706f, 0.13333334f, 0.14901961f, 1.0f);
-    style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.19607843f, 0.1764706f, 0.54509807f, 1.0f);
-    style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.23529412f, 0.21568628f, 0.59607846f, 1.0f);
-    style.Colors[ImGuiCol_Tab] = ImVec4(0.047058824f, 0.05490196f, 0.07058824f, 1.0f);
-    style.Colors[ImGuiCol_TabHovered] = ImVec4(0.11764706f, 0.13333334f, 0.14901961f, 1.0f);
-    style.Colors[ImGuiCol_TabActive] = ImVec4(0.09803922f, 0.105882354f, 0.12156863f, 1.0f);
-    style.Colors[ImGuiCol_TabUnfocused] = ImVec4(0.047058824f, 0.05490196f, 0.07058824f, 1.0f);
-    style.Colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.078431375f, 0.08627451f, 0.101960786f, 1.0f);
-    style.Colors[ImGuiCol_PlotLines] = ImVec4(0.52156866f, 0.6f, 0.7019608f, 1.0f);
-    style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(0.039215688f, 0.98039216f, 0.98039216f, 1.0f);
-    style.Colors[ImGuiCol_PlotHistogram] = ImVec4(1.0f, 0.2901961f, 0.59607846f, 1.0f);
-    style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(0.99607843f, 0.4745098f, 0.69803923f, 1.0f);
-    style.Colors[ImGuiCol_TableHeaderBg] = ImVec4(0.047058824f, 0.05490196f, 0.07058824f, 1.0f);
-    style.Colors[ImGuiCol_TableBorderStrong] = ImVec4(0.047058824f, 0.05490196f, 0.07058824f, 1.0f);
-    style.Colors[ImGuiCol_TableBorderLight] = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
-    style.Colors[ImGuiCol_TableRowBg] = ImVec4(0.11764706f, 0.13333334f, 0.14901961f, 1.0f);
-    style.Colors[ImGuiCol_TableRowBgAlt] = ImVec4(0.09803922f, 0.105882354f, 0.12156863f, 1.0f);
-    style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.23529412f, 0.21568628f, 0.59607846f, 1.0f);
-    style.Colors[ImGuiCol_DragDropTarget] = ImVec4(0.49803922f, 0.5137255f, 1.0f, 1.0f);
-    style.Colors[ImGuiCol_NavHighlight] = ImVec4(0.49803922f, 0.5137255f, 1.0f, 1.0f);
-    style.Colors[ImGuiCol_NavWindowingHighlight] = ImVec4(0.49803922f, 0.5137255f, 1.0f, 1.0f);
-    style.Colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.19607843f, 0.1764706f, 0.54509807f, 0.5019608f);
-    style.Colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.19607843f, 0.1764706f, 0.54509807f, 0.5019608f);
 }
 
 static void AssignAssetToProperty(Object* owner, PropertyOwnerType ownerType, Property& prop, uint32_t index, Asset* newAsset)
@@ -4127,7 +4041,7 @@ static void DrawViewportPanel()
         ImGuiWindowFlags_NoSavedSettings; /* | ImGuiWindowFlags_AlwaysAutoResize*/ /*| ImGuiWindowFlags_NoBackground*/;
 
     ImGui::SetNextWindowPos(ImVec2(viewportBarX, 0.0f));
-    ImGui::SetNextWindowSize(ImVec2(viewportBarWidth, kViewportBarHeight));
+    ImGui::SetNextWindowSize(ImVec2(viewportBarWidth, GetViewportBarHeight()));
 
     ImGui::Begin("Viewport", nullptr, kViewportWindowFlags);
 
@@ -4937,7 +4851,7 @@ static void DrawPaintColorsPanel()
     const float dispWidth = ImGui::GetIO().DisplaySize.x;
     const float dispHeight = ImGui::GetIO().DisplaySize.y;
 
-    ImGui::SetNextWindowPos(ImVec2(kSidePaneWidth + 10.0f, 40.0f));
+    ImGui::SetNextWindowPos(ImVec2(kSidePaneWidth + 10.0f, GetViewportBarHeight()));
     ImGui::SetNextWindowSize(ImVec2(250.0f, 170.0f));
 
     ImGui::Begin("Paint Colors", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
@@ -4963,7 +4877,7 @@ static void DrawPaintInstancesPanel()
     const float dispWidth = ImGui::GetIO().DisplaySize.x;
     const float dispHeight = ImGui::GetIO().DisplaySize.y;
 
-    ImGui::SetNextWindowPos(ImVec2(kSidePaneWidth + 10.0f, 40.0f));
+    ImGui::SetNextWindowPos(ImVec2(kSidePaneWidth + 10.0f, GetViewportBarHeight()));
     ImGui::SetNextWindowSize(ImVec2(250.0f, 210.0f));
 
     ImGui::Begin("Paint Instances", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
@@ -5618,9 +5532,6 @@ void EditorImguiInit()
     // Setup Dear ImGui default style
     ImGui::StyleColorsDark();
 
-    // TODO: Unlock theming when you and the world is ready.
-        // SetupImGuiStyle();
-    //h
     if (GetFeatureFlagsEditor().mShowTheming == true) {
        
         std::string fontPath = ResolveEditorFontPath();
@@ -5641,9 +5552,9 @@ void EditorImguiInit()
 
     // Override theme
     ImVec4* colors = ImGui::GetStyle().Colors;
-    colors[ImGuiCol_Tab] = ImVec4(0.20f, 0.20f, 0.68f, 1.00f);
-    colors[ImGuiCol_TabHovered] = ImVec4(0.26f, 0.61f, 0.98f, 0.80f);
-    colors[ImGuiCol_TabActive] = ImVec4(0.12f, 0.50f, 0.47f, 1.00f);
+    colors[ImGuiCol_Tab] = kBgInactive;
+    colors[ImGuiCol_TabHovered] = kBgHover;
+    colors[ImGuiCol_TabActive] = kSelectedColor;
 
 
 
@@ -5770,9 +5681,9 @@ void EditorImguiGetViewport(uint32_t& x, uint32_t& y, uint32_t& width, uint32_t&
         float invScale = 1.0f / scale;
 
         x = 0;
-        y = uint32_t(kViewportBarHeight * scale + 0.5f);
+        y = uint32_t(GetViewportBarHeight() * scale + 0.5f);
         int32_t iWidth = (int32_t)io.DisplaySize.x;
-        int32_t iHeight = int32_t(io.DisplaySize.y - kViewportBarHeight * scale + 0.5f);
+        int32_t iHeight = int32_t(io.DisplaySize.y - GetViewportBarHeight() * scale + 0.5f);
 
         if (GetEditorState()->mShowLeftPane)
         {
